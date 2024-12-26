124005a9fc64526ea32d0b030b074ce12bfaef98
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2024-12-26 16:47:29 -0700

    Venue Card Component Created

---
+++ components/VenueCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const VenueCard = () => {
+  return (
+    <View>
+      <Text>VenueCard</Text>
+    </View>
+  )
+}
+
+export default VenueCard
+
+const styles = StyleSheet.create({})
