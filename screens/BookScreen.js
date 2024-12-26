2945b3e4985f3f1d7deb69c65d6eea49c8e6698b
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2024-12-26 10:32:09 -0700

    using Safe view compatible with both android and IOS

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { SafeAreaView } from 'react-native-safe-area-context';
 const BookScreen = () => {
   return (
-    <View>
-      <Text>BookScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{flex: 1, backgroundColor: '#f5f5f5'}}>
+      <View>
+        <View style={{flexDirection: 'row', alignItems: 'center', gap:5}}>
+          <Text>Shahkar Nagar</Text>
+          <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default BookScreen
+export default BookScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
