7e0d96ff0038d4942ef373e411b2a0fd12b7ad51
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2025-01-01 10:16:20 -0700

    NAme Screen added

--- screens/NameScreen.js
+++ screens/NameScreen.js
@@ -1,14 +1,109 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
 
 const NameScreen = () => {
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const navigation = useNavigation();
+
+  const saveName = () => {
+    navigation.navigate('Image');
+  };
   return (
-    <View>
-      <Text>NameScreen</Text>
-    </View>
-  )
-}
+    <>
+      <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+        <View style={{marginHorizontal: 10}}>
+          <Ionicons name="arrow-back" size={24} color="black" />
+        </View>
 
-export default NameScreen
+        <View style={{marginHorizontal: 10, marginVertical: 15}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+            Complete Your Profile
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: 10,
+              gap: 5,
+            }}>
+            <Text
+              style={{
+                color: 'gray',
+                alignContent: 'center',
+                justifyContent: 'center',
+              }}>
+              What would you like your mates to call you?
+            </Text>
+            <Ionicons name="heart" size={20} color="red" />
+          </View>
+        </View>
 
-const styles = StyleSheet.create({})
+        <View
+          style={{
+            backgroundColor: 'white',
+            marginHorizontal: 10,
+            marginVertical: 25,
+            gap: 20,
+            flexDirection: 'column',
+          }}>
+          <View>
+            <Text style={{fontSize: 16, color: 'gray'}}>First Name *</Text>
+            <TextInput
+              value={firstName}
+              onChangeText={setFirstName}
+              style={{
+                padding: 10,
+                borderColor: '#D0D0D0',
+                borderWidth: 1,
+                borderRadius: 10,
+                marginTop: 10,
+              }}
+            />
+          </View>
+          <View>
+            <Text style={{fontSize: 16, color: 'gray'}}>Last Name </Text>
+            <TextInput
+              value={lastName}
+              onChangeText={setLastName}
+              style={{
+                padding: 10,
+                borderColor: '#D0D0D0',
+                borderWidth: 1,
+                borderRadius: 10,
+                marginTop: 10,
+              }}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+      <Pressable
+        onPress={saveName}
+        style={{
+          backgroundColor: '#07bc0c',
+          padding: 12,
+          marginTop: 'auto',
+          marginBottom: 30,
+          marginHorizontal: 10,
+          borderRadius: 4,
+        }}>
+        <Text
+          style={{
+            textAlign: 'center',
+            color: 'white',
+            fontWeight: '500',
+            fontSize: 15,
+          }}>
+          Next
+        </Text>
+      </Pressable>
+    </>
+  );
+};
+
+export default NameScreen;
+
+const styles = StyleSheet.create({});
